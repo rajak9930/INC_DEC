e46fca241da60c68c918c1ca299ccdb265d51f0c
rajak9930 <81839703+rajak9930@users.noreply.github.com>
2022-09-18 11:17:43 +0530

    react-calculator
    react-calculator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import Count from './INC_DEC/Count';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-function App() {
-  
-  return (
-   
-    <div className="App">
-      <Count/>
+import Calculator from './Calculator/Calculator';
+import Task from './TaskViewer/Task';
+//import Calculator from './Calculator/Calculator';
 
+
+function App() {
+  return (
+    <div className="App">
+  <Calculator/>
+     
+      
+    
+      
+    
     </div>
   );
-
-  }
-
+}
 
 export default App;
